b4c9e5da8758eabb686d3eb319c7ced01ce9964b
Mayssae El Ahmadie <mayssae.ahmadie@gmail.com>
2023-10-23 12:33:50 +0300

    Static Teachers

--- src/Components/Teachers.jsx
+++ src/Components/Teachers.jsx
@@ -6,17 +6,21 @@ function Teachers() {
         <div>
             <h3 className="Teachers-heading">Teachers</h3>
             <div className="Teachers-grid">
-                <div>
+                <div className='Teachers-background'>
                     <img className="Teachers-image" src="../Images/Teacher_1.jpg" alt="" />
+                    <p className='Teachers-name'> Ms. W </p>
                 </div>
                 <div>
                     <img className="Teachers-image" src="../Images/Teacher_2.png" alt="" />
+                    <p className='Teachers-name'> Ms. X </p>
                 </div>
                 <div>
                     <img className="Teachers-image1" src="../Images/Teacher_4.jpg" alt="" />
+                    <p className='Teachers-name'> Mr. Y </p>
                 </div>
                 <div>
                     <img className="Teachers-image" src="../Images/Teacher_6.png" alt="" />
+                    <p className='Teachers-name'> Mr. X </p>
                 </div>
             </div>
         </div>
